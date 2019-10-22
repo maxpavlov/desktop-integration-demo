ede50aa6c66f4175e03d2da1b274993fb012358a
Max Pavlov <max@radacode.com>
2019-10-22 13:20:22 +0300

    Adding pre-developed sources.

---
+++ webclient/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="PatientList">
+        <h2>Patients</h2>
+      </div>
+      <div className="HeartMeasurements">
+        <h2>Heart measurements</h2>
+      </div>
+      <div className="EyeSight">
+        <h2>Eyesight measurements</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
